befe0a76e195072eb38caefbea7608993b80594e
Sanskruti Mahure <123569648+sanskrutimahure@users.noreply.github.com>
2023-01-28 10:47:18 +0530

    Create QuestionAnswer.java

---
+++ QuizApp/QuestionAnswer.java
@@ -0,0 +1,46 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+        public static String question[]={
+                
+                "Who invented the BALLPOINT PEN ?",
+                "In which decade was the first solid state integrated circuit demonstrated?",
+                "What J. B. Dunlop invented?",
+                "Which scientist discovered the radioactive element radium?",
+                "When was barb wire patented?",
+                "What is the name of the CalTech seismologist who invented the scale used to measure the magnitude of earthquakes?\n",
+                "What Galileo invented?",
+                "This statesman, politican, scholar, inventor, and one of early presidents of USA invented the swivel chair, the spherical sundial, the moldboard plow, and the cipher wheel.\n",
+                "What James Watt invented?",
+                "Where is the village of Branston, after which the famous pickle is named?"
+             
+        };
+        public static String choices[][]={
+
+                {"Biro Brothers" ,"Waterman Brothers" ,"Bicc Brothers" ,"Write Brothers"},
+                {"1950s" ,"1960s" ,"1970s" ,"1980s"},
+                {"Pneumatic rubber tire" ,"Automobile wheel rim" ,"Rubber boot" ,"Model airplanes"},
+                {"Isaac Newton" ,"Albert Einstein" ,"Benjamin Franklin" ,"Marie Curie"},
+                {"1874" ,"1840" ,"1895" ,"1900"},
+                {"Charles Richter" ,"Hiram Walker" ,"Giuseppe Mercalli" ,"Joshua Rumble"},
+                {"Barometer" ,"Pendulum clock" ,"Microscope" ,"Thermometer"},
+                {"George Washington" ,"Alexander Hamilton" ,"John Adams","Thomas Jefferson"},
+                {"Diving bell" ,"Steam boat" ,"Hot air balloon" ,"Rotary steam engine"},
+                {"Yorkshire" ,"Lancashire" ,"Staffordshire" ,"Norfolk"}
+            
+        };
+        public static String correct[]={
+              
+                "Biro Brothers",
+                "1950s",
+                "Pneumatic rubber tire",
+                "Marie Curie",
+                "1874",
+                "Charles Richter",
+                "Thermometer",
+                "Thomas Jefferson",
+                "Rotary steam engine",
+                "Staffordshire"
+                
+        };
+    }
